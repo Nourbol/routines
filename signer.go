16142741d8706f82771bb78304118537fa88c382
Nurbol Bektibay <nurbol.bektibay@intexsoft.kz>
2023-02-11 00:53:45 +0600

    Made this code better.

--- signer.go
+++ signer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"strconv"
 	"strings"
@@ -8,75 +9,114 @@ import (
 )
 
 func SingleHash(in, out chan interface{}) {
-	var wg sync.WaitGroup
-	data := (<-in).(string)
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
 
-	firstPartChannel := make(chan string)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		firstPartChannel <- DataSignerCrc32(data)
-	}()
+	for input := range in {
 
-	secondPartChannel := make(chan string)
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		secondPartChannel <- DataSignerCrc32(DataSignerMd5(data))
-	}()
+		wg.Add(1)
+
+		go func(input interface{}) {
+			defer wg.Done()
+
+			data := strconv.Itoa(input.(int))
+			crc32chan := make(chan string)
+			crc32md5chan := make(chan string)
+
+			go func(crc32chan chan string, data string) {
+				crc32chan <- DataSignerCrc32(data)
+			}(crc32chan, data)
+
+			mu.Lock()
+			md5 := DataSignerMd5(data)
+			mu.Unlock()
+
+			go func(crc32md5chan chan string, md5 string) {
+				crc32md5chan <- DataSignerCrc32(md5)
+			}(crc32md5chan, md5)
+
+			crc32 := <-crc32chan
+			crc32md5 := <-crc32md5chan
+
+			fmt.Printf("%s SingleHash data %s\n", data, data)
+			fmt.Printf("%s SingleHash md5(data) %s\n", data, md5)
+			fmt.Printf("%s SingleHash crc32(md5(data)) %s\n", data, crc32md5)
+			fmt.Printf("%s SingleHash crc32(data) %s\n", data, crc32)
+			fmt.Printf("%s SingleHash result %s\n", data, crc32+"~"+crc32md5)
+
+			result := crc32 + "~" + crc32md5
+			out <- result
+
+		}(input)
+
+	}
 
 	wg.Wait()
-	out <- <-firstPartChannel + "~" + <-secondPartChannel
 }
 
 func MultiHash(in, out chan interface{}) {
 	var wg sync.WaitGroup
-	data := (<-in).(string)
+	var mu sync.Mutex
+	var array [6]string
 
-	hashedValues := make([]string, 6)
-	for i := 0; i < 6; i++ {
+	for input := range in {
 		wg.Add(1)
-		go func(i int) {
+
+		go func(input interface{}) {
 			defer wg.Done()
-			hashedValues[i] = DataSignerCrc32(strconv.Itoa(i) + data)
-		}(i)
+			var wgInner sync.WaitGroup
+			data := input.(string)
+			for th := 0; th < 6; th++ {
+				wgInner.Add(1)
+				go func(th int) {
+					defer wgInner.Done()
+					tmp := DataSignerCrc32(strconv.Itoa(th) + data)
+					mu.Lock()
+					array[th] = tmp
+					mu.Unlock()
+				}(th)
+			}
+			wgInner.Wait()
+			result := strings.Join(array[:], "")
+			out <- result
+		}(input)
 	}
+
 	wg.Wait()
-	out <- strings.Join(hashedValues, "")
 }
 
 func CombineResults(in, out chan interface{}) {
-	select {
-	case <-in:
-		return
-	default:
-		results := make([]string, 0)
-
-		for result := range in {
-			results = append(results, result.(string))
-		}
-
-		sort.Strings(results)
-		out <- strings.Join(results, "_")
+	var tmp []string
+	wg := &sync.WaitGroup{}
+	for input := range in {
+		wg.Add(1)
+		go func(input interface{}) {
+			defer wg.Done()
+			tmp = append(tmp, input.(string))
+		}(input)
 	}
+	wg.Wait()
+	sort.Strings(tmp)
+	result := strings.Join(tmp, "_")
+	fmt.Printf("CombineResults \n%s\n", result)
+	out <- result
 }
 
 func ExecutePipeline(jobs ...job) {
-	channels := make([]chan interface{}, len(jobs))
-	for i := range channels {
-		channels[i] = make(chan interface{})
-		defer close(channels[i])
-	}
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{})
+	for _, job := range jobs {
+		wg.Add(1)
 
-	for i, pipe := range jobs {
-		go func(i int, pipe job) {
-			if i == len(channels)-1 {
-				pipe(channels[i], make(chan interface{}))
-				return
-			}
-			pipe(channels[i], channels[i+1])
-		}(i, pipe)
+		out := make(chan interface{})
+		go worker(in, out, job, wg)
+		in = out
 	}
+	wg.Wait()
+}
 
-	channels[0] <- 1
+func worker(in, out chan interface{}, job job, wg *sync.WaitGroup) {
+	defer close(out)
+	defer wg.Done()
+	job(in, out)
 }
